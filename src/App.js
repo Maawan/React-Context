2d713ffc83144159fd80216a921198b31f62ff81
Mawan <maawan18@gmail.com>
2023-06-05 02:33:38 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {Fragment} from 'react'
+import Provider from './provider'
+import Context from "./context"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Agents(){
+    return <AgentOne />
 }
 
+function AgentOne(){
+    return <AgentTwo />
+}
+
+function AgentTwo(){
+    return <AgentBond />
+}
+
+function AgentBond(){
+    return(
+        <Context.Consumer>
+            {
+                (context) => (
+                    <Fragment>
+                        <h3>Agent Information</h3>
+                        <p>Mission Name {context.data.mname}</p>
+                        <h2>Mission Status {context.data.accept}</h2>
+                        <button onClick={context.isMissionAccepted}>Choose to click</button>
+                    </Fragment>
+                )
+            }
+        </Context.Consumer>
+    )
+}
+
+function App(){
+    return(
+        <div>
+            <h1>Context Api</h1>
+            <Provider>
+                <Agents />
+            </Provider>
+        </div>
+    )
+}
 export default App;
